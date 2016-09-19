c44c342b14408413f44fce950d4438f24a29aacb
Bound3R <daniel.naranjo@edunext.co>
2016-09-19 16:38:20 -0500

    Using variables for color
    Using the proxy settings for all the variables

--- openedx_email_extensions/settings.py
+++ openedx_email_extensions/settings.py
@@ -4,7 +4,13 @@ This is the settings file for the openedx_email_extensions application
 """
 
 from path import Path as path
-from django.conf import settings
+
+try:
+    from openedx.conf import settings
+except ImportError:
+    # Fall back to regular settings if there isn't a site aware settings module
+    from django.conf import settings
+
 
 APP_ROOT = path(__file__).abspath().dirname()
 REPO_ROOT = APP_ROOT.dirname()
@@ -21,3 +27,33 @@ COMPATIBILITY = {
     "emails/unenroll_email_allowedmessage.txt": "unenroll_email_allowedmessage.html",
     "emails/unenroll_email_enrolledmessage.txt": "unenroll_email_enrolledmessage.html",
 }
+
+DEFAULT_EXTRA_FOOTER_TEMPLATE = None
+
+DEFAULT_EMAIL_COLORS = {
+  "email_wrapper__background": "#F3F3F3",
+  "email_body__color": "#0A0A0A",
+  "email_body__background": "#FEFEFE",
+  "email_button__color": "#FEFEFE",
+  "email_button__background": "#2DAAE1",
+  "email_button__border": "#2DAAE1",
+  "email_footer__color": "#0A0A0A",
+  "email_footer__color_link": "#2DAAE1",
+  "email_footer__color_small": "#B1B1B1"
+}
+
+class ProxySettings(object):
+    """
+    This object is a proxy for the django.conf settings object. It defaults to a 
+    local variable of this module prefixed with DEFAULT_
+    """
+
+    def __getattr__(self, name):
+        try:
+            return getattr(settings, name)
+        except AttributeError:
+            default_name = "DEFAULT_{}".format(name)
+            return globals().get(default_name)
+
+
+proxy_settings = ProxySettings()
