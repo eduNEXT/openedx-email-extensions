4553212ef97faf3fab201e65b883eaac825a2d20
Felipe Montoya <felipe.montoya@edunext.co>
2018-10-09 11:24:48 -0500

    Bump version: 0.1.5 → 0.1.6

--- openedx_email_extensions/__init__.py
+++ openedx_email_extensions/__init__.py
@@ -4,6 +4,6 @@ A package to make the outgoing emails in open-edx good looking..
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 default_app_config = 'openedx_email_extensions.apps.OpenedxEmailExtensionsConfig'  # pylint: disable=invalid-name
